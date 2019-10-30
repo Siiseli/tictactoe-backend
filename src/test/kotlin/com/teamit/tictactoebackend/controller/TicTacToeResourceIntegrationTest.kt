4a705b842197451b65efeedd449b0e21fdf89ccc
Tino Myllyselkä <tino.myllyselka@teamit.fi>
2019-10-30 13:33:35 +0200

    Properly use junit 5 in tests

--- src/test/kotlin/com/teamit/tictactoebackend/controller/TicTacToeResourceIntegrationTest.kt
+++ src/test/kotlin/com/teamit/tictactoebackend/controller/TicTacToeResourceIntegrationTest.kt
@@ -5,16 +5,16 @@ import junit.framework.TestCase.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.junit.Assert.assertNotEquals
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TicTacToeResourceIntegrationTest {
 
