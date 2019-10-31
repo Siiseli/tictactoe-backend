572e65b62416097306a29877ef1d99302fa740cd
Tino Myllyselkä <tino.myllyselka@teamit.fi>
2019-10-31 09:07:04 +0200

    Add tests for AI service

---
+++ src/test/kotlin/com/teamit/tictactoebackend/service/TicTacToeAIServiceTests.kt
@@ -0,0 +1,22 @@
+package com.teamit.tictactoebackend.service
+
+import com.teamit.tictactoebackend.model.game.TicTacToeCharacters
+import com.teamit.tictactoebackend.model.game.TicTacToeGame
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicTacToeAIServiceTests {
+
+    @Autowired
+    private lateinit var ai: TicTacToeAIService
+
+    @Test
+    fun shouldMakeMove() {
+        val game = TicTacToeGame("testId", "testName", TicTacToeCharacters.X, TicTacToeCharacters.O)
+        val updatedBoard = ai.makeMove(game)
+        assertNotEquals(game.board, updatedBoard)
+    }
+}
